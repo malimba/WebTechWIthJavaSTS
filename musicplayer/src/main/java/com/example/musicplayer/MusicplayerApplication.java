e269f3d9914ff181e1d3834ad39431c1d1ceeace
malimba <malimbageorgeyandanbon@gmail.com>
2025-07-13 14:51:08 +0000

    configured dotenv

--- musicplayer/src/main/java/com/example/musicplayer/MusicplayerApplication.java
+++ musicplayer/src/main/java/com/example/musicplayer/MusicplayerApplication.java
@@ -3,10 +3,13 @@ package com.example.musicplayer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class MusicplayerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(MusicplayerApplication.class, args);
 	}
 
