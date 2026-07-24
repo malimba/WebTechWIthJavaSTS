d5d45abbd754567d9f32cd5387b79144c3ca009e
malimba <44820067+malimba@users.noreply.github.com>
2026-07-24 20:47:28 +0000

    Update MusicplayerApplicationTests.java

--- musicplayer/src/test/java/com/example/musicplayer/MusicplayerApplicationTests.java
+++ musicplayer/src/test/java/com/example/musicplayer/MusicplayerApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.musicplayer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// modified test
 @SpringBootTest
 class MusicplayerApplicationTests {
 
