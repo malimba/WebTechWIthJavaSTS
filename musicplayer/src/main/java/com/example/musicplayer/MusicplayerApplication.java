7e5e754d948f9e66ec07c6ec80f6e4814b02d03d
malimba <44820067+malimba@users.noreply.github.com>
2026-07-24 20:48:36 +0000

    Update MusicplayerApplication.java

--- musicplayer/src/main/java/com/example/musicplayer/MusicplayerApplication.java
+++ musicplayer/src/main/java/com/example/musicplayer/MusicplayerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.cdimascio.dotenv.Dotenv;
-
+// man sprbt tie
 @SpringBootApplication
 public class MusicplayerApplication {
 
